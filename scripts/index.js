51288a105fbdf6b7e9b1f09a5fdc2768c7549cf1
Анна Ларионова <Larionova@bro.local>
2023-03-21 11:58:39 +0300

    hotfix: осталось отредактировать только папку blocks

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,18 @@ const editPopup = document.querySelector(".popup");
 const editPopupCloseButton = editPopup.querySelector(".popup__close-button");
 const nameInput = editPopup.querySelector(".popup__text_type_name");
 const interestsInput = editPopup.querySelector(".popup__text_type_interests");
-const editPopupSubmit = editPopup.querySelector(".popup__submit-btn");
-const editPopupForm = editPopup.querySelector(".popup__form");
+const editPopupForm = editPopup.querySelector(".popup__container");
 const profileName = document.querySelector(".profile__title");
 const profileInterests = document.querySelector(".profile__subtitle");
 
-console.log(editPopupCloseButton)
-
 editButton.addEventListener('click', () => {
-  editPopup.classList.add("popup_open");
+  editPopup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   interestsInput.value = profileInterests.textContent;
 });
 
 editPopupCloseButton.addEventListener('click', () => {
-  editPopup.classList.remove("popup_open");
+  editPopup.classList.remove("popup_opened");
 });
 
 editPopupForm.addEventListener('submit', (event) => {
@@ -27,5 +24,5 @@ editPopupForm.addEventListener('submit', (event) => {
 
   profileName.textContent = name;
   profileInterests.textContent = interests;
-  editPopup.classList.remove("popup_open");
+  editPopup.classList.remove("popup_opened");
 });
